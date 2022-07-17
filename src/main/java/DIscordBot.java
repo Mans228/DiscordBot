5875007701a2411d949c69666279ab35cd73fdf5
Mans228 <zhenismansur@gmail.com>
2022-07-17 22:01:33 +0200

    Initial commit

---
+++ src/main/java/DIscordBot.java
@@ -0,0 +1,29 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import javax.security.auth.login.LoginException;
+import java.util.Random;
+
+public class DIscordBot extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        JDA bot= JDABuilder.createDefault("token")
+                .addEventListeners(new DIscordBot())
+                .setActivity(Activity.playing("в очко Рафа"))
+                .build();
+        bot.upsertCommand("roll","Gives a randon number between 1 and 100").queue();
+
+    }
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        Random r= new Random();
+        int randomint=r.nextInt(100)+1;
+        if (event.getName().equals("roll")){
+            event.reply(String.valueOf(randomint)).queue();
+        }
+    }
+}
